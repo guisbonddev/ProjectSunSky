1b210800e2f84a9ad825bb005ac29d9a95a39f10
guisbonddev <wguisbond@gmail.com>
2018-08-17 10:14:02 -0400

    created migrations for finance module

---
+++ database/migrations/2018_08_17_140444_create_flight_finances_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlightFinancesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flight_finances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cfi');
+            $table->integer('ins_rate');
+            $table->integer('ins_time');
+            $table->integer('pilot1');
+            $table->integer('pilot2');
+            $table->integer('flight');
+            $table->integer('total_time');
+            $table->integer('aircraft');
+            $table->integer('aircraft_rate');
+            $table->integer('misc_costs');
+            $table->integer('total_cost');
+            $table->integer('billing_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flight_finances');
+    }
+}
