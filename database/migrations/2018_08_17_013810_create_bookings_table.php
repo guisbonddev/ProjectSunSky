4600a9050e17313c0f4988dd73474518cde34ca8
guisbonddev <wguisbond@gmail.com>
2018-08-16 21:44:46 -0400

    created migrations for schedule module (base)

---
+++ database/migrations/2018_08_17_013810_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('aircraft');
+            $table->datetime('date_out');
+            $table->datetime('date_in');
+            $table->integer('cfi');
+            $table->integer('other_resources');
+            $table->integer('pilot');
+            $table->string('type');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
